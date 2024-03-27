7b7ddd1b2a272ac32a87abc58b5327eb67ba690f
RohitkumarA <rohitasare7@gmail.com>
2024-03-27 12:06:09 +0530

    reset

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
