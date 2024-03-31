21416a2bd5f07b6623123fea104caba15056996d
RohitkumarA <rohitasare7@gmail.com>
2024-03-31 11:05:22 +0530

    adding dist

--- src/pages/components/ToastList.vue
+++ src/pages/components/ToastList.vue
@@ -9,8 +9,8 @@ function remove(index) {
 </script>
 <template>
 
-    <TransitionGroup tag="div" enter-from-class="translate-x-full opacity-0" enter-active-class="duration-500"
-        leave-active-class="duration-500" leave-to-class="translate-x-full opacity-0"
+    <TransitionGroup tag="div" enter-from-class="translate-x-full opacity-0" enter-active-class="transition-all duration-500"
+        leave-active-class="transition-all duration-500" leave-to-class="translate-x-full opacity-0"
         class="fixed top-8 right-8 z-90 w-full max-w-xs space-y-4 toastList">
         <ToastListItem v-for="(item, index) in toast.items" :key="item.key" :message="item.message" :type="item.type"
             :duration="5500" @remove="remove(index)" />
