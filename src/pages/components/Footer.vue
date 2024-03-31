d5374bb30db8fc788f1c959d5fafed9a9c613364
RohitkumarA <rohitasare7@gmail.com>
2024-03-31 15:04:04 +0530

    adding dist new changes in footer

--- src/pages/components/Footer.vue
+++ src/pages/components/Footer.vue
@@ -49,11 +49,30 @@
                     <path fill="#FFF" d="M20 31L20 17 32 24z"></path>
                 </svg>
             </a>
+            <a href="https://www.youtube.com/@ThatSalesforceGuy" target="_blank"
+                class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 512 512"
+                    id="twitter">
+                    <g clip-path="url(#clip0_84_15697)">
+                        <rect width="512" height="512" fill="#000" rx="60"></rect>
+                        <path fill="#fff"
+                            d="M355.904 100H408.832L293.2 232.16L429.232 412H322.72L239.296 302.928L143.84 412H90.8805L214.56 270.64L84.0645 100H193.28L268.688 199.696L355.904 100ZM337.328 380.32H366.656L177.344 130.016H145.872L337.328 380.32Z">
+                        </path>
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_84_15697">
+                            <rect width="512" height="512" fill="#fff"></rect>
+                        </clipPath>
+                    </defs>
+                </svg>
+            </a>
+
+
             <a href="https://forms.gle/aVC82TBjCeQjUCrDA" target="_blank"
                 class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white text-sm">
                 Request Feature
             </a>
-            <p class="text-gray-500 dark:text-gray-400 text-sm">Please consider bookmarking this URL for convenient and quick access in the future.</p>
+            <!-- <p class="text-gray-500 dark:text-gray-400 text-sm">Please consider bookmarking this URL for convenient and quick access in the future.</p> -->
         </div>
 
         <!-- <ul class="flex flex-wrap items-center mb-6 space-y-1 md:mb-0">
@@ -62,7 +81,7 @@
             <a href="mailto:thatsfdcguy@gmail.com" class="text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Contact</a>
         </ul> -->
 
-        <p class="text-sm text-center text-gray-500 dark:text-gray-400 mt-4 md:mt-0 flex items-center justify-center">
+        <!-- <p class="text-sm text-center text-gray-500 dark:text-gray-400 mt-4 md:mt-0 flex items-center justify-center">
             Built with
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="inline-block mr-1 ml-1">
                 <path fill="#f05542"
@@ -72,7 +91,13 @@
             by 
             <a href="https://www.youtube.com/@ThatSalesforceGuy" target="_blank"
                 class="font-semibold dark:text-gray-300 ml-1">That Salesforce Guy</a>
-        </p>
+        </p> -->
+
+        <p class="text-sm text-center text-gray-500 dark:text-gray-400 mt-4 md:mt-0">Hey Curious 👋 I'm <a
+                href="https://rohitasare7.github.io" target="_blank"
+                class="font-semibold dark:text-gray-300 underline">Rohitkumar</a>, the creator of Random Data Generator.
+            You can follow my work on <a href="https://twitter.com/salesforceguy" target="_blank"
+                class="font-semibold dark:text-gray-300 underline ml-1">Twitter.</a></p>
     </footer>
 
 </template>
