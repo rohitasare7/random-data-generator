baafde9b2e478795d717c7ecd4ca6728b67d361c
Rohit Asare <rohit.asare@one.nz>
2025-03-23 21:27:19 +1300

    Added refresh icon

--- src/pages/Faker.vue
+++ src/pages/Faker.vue
@@ -78,8 +78,9 @@ const generateRandomString = (pattern) => {
 
 const randomData = ref({});
 const selectedGender = ref('male');
-
+const isRandomDataLoading = ref(false);
 const updateRandomData = () => {
+  isRandomDataLoading.value = true;
   //Define Constants
   const gender = getValue('person.sex');
   const firstName = getValue('person.firstName', [gender]);//fakerInstance.value.person.firstName();
@@ -131,6 +132,10 @@ const updateRandomData = () => {
     // Add more items as needed
   }
   randomData.value = data;
+
+  setTimeout(() => {
+    isRandomDataLoading.value = false;
+  }, 700);
 }
 
 const formatDisplayName = (text) => {
@@ -199,8 +204,14 @@ const saveLocaleToStorage = () => {
 
   <div class="container mx-auto pb-4">
     <div class="text-center justify-center">
-      <PrimaryButton @click="updateRandomData" class="my-4">
+      <PrimaryButton @click="updateRandomData" class="my-4" :title="'Click to Generate Unlimited Random Data'">
         Generate Data
+        <svg xmlns="http://www.w3.org/2000/svg" :class="['ml-2', { 'animate-spin': isRandomDataLoading }]" width="24"
+          height="24"
+          viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
+          <path fill="currentColor"
+            d="M12 20q-3.35 0-5.675-2.325T4 12t2.325-5.675T12 4q1.725 0 3.3.712T18 6.75V5q0-.425.288-.712T19 4t.713.288T20 5v5q0 .425-.288.713T19 11h-5q-.425 0-.712-.288T13 10t.288-.712T14 9h3.2q-.8-1.4-2.187-2.2T12 6Q9.5 6 7.75 7.75T6 12t1.75 4.25T12 18q1.7 0 3.113-.862t2.187-2.313q.2-.35.563-.487t.737-.013q.4.125.575.525t-.025.75q-1.025 2-2.925 3.2T12 20" />
+        </svg>
       </PrimaryButton>
     </div>
 
